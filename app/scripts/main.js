a2eabf7183362775680723439f2b811dcc303c31
Michael Bonfiglio <michaelbonfiglio@Macintosh.home>
2015-07-17 15:45:15 -0400

    Adding files to Github
    Creating project

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+/* jshint devel:true */
+console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+
+	$('#button1').click(function() {
+	  $('#img-version-2').attr("src","images/V2-desktop_A.png");
+	});
+
+    $("#button2").click(function(){
+      $('#img-version-2').attr("src","images/V2-desktop_B.png");
+    });
+
+    $("#button3").click(function(){
+      $('#img-version-2').attr("src","images/V2-desktop_C.png");
+    });
+});
+
+
