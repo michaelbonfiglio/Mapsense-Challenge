302b4a40425b6f039ba31b6b0cbd3c174fcb80b3
Michael Bonfiglio <michaelbonfiglio@Macintosh.home>
2015-07-17 16:34:19 -0400

    fixed js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,17 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+'use strict';
 
 $(document).ready(function(){
 
-	$('#button1').click(function() {
-	  $('#img-version-2').attr("src","images/V2-desktop_A.png");
+	$('#button1').click(function(){
+	  $('#img-version-2').attr('src','images/V2-desktop_A.png');
 	});
 
-    $("#button2").click(function(){
-      $('#img-version-2').attr("src","images/V2-desktop_B.png");
+    $('#button2').click(function(){
+      $('#img-version-2').attr('src','images/V2-desktop_B.png');
     });
 
-    $("#button3").click(function(){
-      $('#img-version-2').attr("src","images/V2-desktop_C.png");
+    $('#button3').click(function(){
+      $('#img-version-2').attr('src','images/V2-desktop_C.png');
     });
 });
 
